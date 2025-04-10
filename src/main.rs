31228ac6967387208de2cfdbb4c78837718601f4
lordBman <okelekelenobel@gmail.com>
2025-04-10 08:45:10 +0100

    using labels

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use vizia::{Application, ApplicationError};
 use vizia::prelude::*;
 
 fn main() -> Result<(), ApplicationError> {
-    Application::new(|_cx|{
-       
+    Application::new(|cx|{
+       Label::new(cx, "First Label")
+            .font_size(32.0)
+            .color("#3367ff")
+            .font_weight(300).padding(Pixels(20.0));
     }).title("First Vizia").min_inner_size(Some((800, 480))).run()    
 }
